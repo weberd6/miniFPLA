4161ada1ec50f166b25bc698bf9ac4ce8359b691
Doug Weber <weberd6@mail.uc.edu>
2014-11-15 20:53:09 -0500

    Some minor modifications

--- IRSim/FPLA_inout.cmd
+++ IRSim/FPLA_inout.cmd
@@ -1,4 +1,4 @@
-stepsize 10
+stepsize 20
 vector Clock Clk
 vector Rst Reset
 vector C Cfg
@@ -6,7 +6,7 @@ vector T Test
 vector In In8 In7 In6 In5 In4 In3 In2 In1 In0
 vector Out Out8 Out7 Out6 Out5 Out4 Out3 Out2 Out1 Out0
 
-ana Clk Cfg Test Reset In8 In7 In6 In5 In4 In3 In2 In1 In0 Out8 Out7 Out6 Out5 Out4 Out3 Out2 Out1 Out0 test_vdd test_gnd
+ana Clk Cfg Test Reset In8 In7 In6 In5 In4 In3 In2 In1 In0 Out8 Out7 Out6 Out5 Out4 Out3 Out2 Out1 Out0 Gnd_out Vdd_out Cfg_out Clk_out Clk_out1
 
 setvector Clock 1
 s
@@ -20,90 +20,92 @@ setvector Clock 1
 s
 setvector C 1
 setvector Rst 0
-setvector In 
+setvector In 000000000
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 111111111
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 111111111
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 000000000
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 000000000
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 111111111
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 111111111
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 000000000
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 000000000
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 111111111
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 111111111
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 000000000
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 000000000
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 111111111
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 111111111
 setvector Clock 0
 s
 setvector Clock 1
 s
-setvector In 
+setvector In 000000000
 setvector Clock 0
 s
 setvector Clock 1
 s
+setvector C 0
 setvector Clock 0
 s
 setvector Clock 1
 s
+setvector In 000000000
 setvector Clock 0
 s
 setvector Clock 1
